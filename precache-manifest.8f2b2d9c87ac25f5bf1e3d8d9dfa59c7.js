c92848bba740c1ade5e69a5ea39cdcd960ff7e44
Malfit <oleksandr.rybakov@nure.ua>
2020-04-15 16:42:21 +0300

    Updates

---
+++ precache-manifest.8f2b2d9c87ac25f5bf1e3d8d9dfa59c7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3e21e3b29fe63205dce38018dd00c3",
+    "url": "/Cursor-React-Homework4/index.html"
+  },
+  {
+    "revision": "b009b44adaa30ebf21e6",
+    "url": "/Cursor-React-Homework4/static/css/main.b48a3219.chunk.css"
+  },
+  {
+    "revision": "c8a32b95d0442d2d75e9",
+    "url": "/Cursor-React-Homework4/static/js/2.a705fb09.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Cursor-React-Homework4/static/js/2.a705fb09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b009b44adaa30ebf21e6",
+    "url": "/Cursor-React-Homework4/static/js/main.e7513ba1.chunk.js"
+  },
+  {
+    "revision": "c08e113763d51537707f",
+    "url": "/Cursor-React-Homework4/static/js/runtime-main.4e936161.js"
+  },
+  {
+    "revision": "8a491c1fca12fb5d1dac63e2d543e9bd",
+    "url": "/Cursor-React-Homework4/static/media/10_10.8a491c1f.jpg"
+  },
+  {
+    "revision": "aebe64bc506c036344d9b7c3d9d4be2d",
+    "url": "/Cursor-React-Homework4/static/media/11_11.aebe64bc.jpg"
+  },
+  {
+    "revision": "70d0c6d693e9540e4118fef6cc0780be",
+    "url": "/Cursor-React-Homework4/static/media/12_12.70d0c6d6.jpg"
+  },
+  {
+    "revision": "2c9c69e9d76037229b7cf2e5a7e4f8a4",
+    "url": "/Cursor-React-Homework4/static/media/13_13.2c9c69e9.jpg"
+  },
+  {
+    "revision": "e24c79ca3c3f2a30fc546c057a7c3be0",
+    "url": "/Cursor-React-Homework4/static/media/14_14.e24c79ca.jpg"
+  },
+  {
+    "revision": "b12e1a9ff3d90c1b0bbdb13f765621a0",
+    "url": "/Cursor-React-Homework4/static/media/15_15.b12e1a9f.jpg"
+  },
+  {
+    "revision": "c94e445e1d18a3e5f2ffda0cdcb506ed",
+    "url": "/Cursor-React-Homework4/static/media/1_1.c94e445e.jpg"
+  },
+  {
+    "revision": "2a0cf852b55bfc80aa2f211d5ec2d83b",
+    "url": "/Cursor-React-Homework4/static/media/2_2.2a0cf852.jpg"
+  },
+  {
+    "revision": "98827911f2f5fca814e2719d1e5157b4",
+    "url": "/Cursor-React-Homework4/static/media/3_3.98827911.jpg"
+  },
+  {
+    "revision": "9c1b985596ab8fb6ac28c25e4ee56c56",
+    "url": "/Cursor-React-Homework4/static/media/4_4.9c1b9855.jpg"
+  },
+  {
+    "revision": "93283ca42a39e734d447e0840ffa475c",
+    "url": "/Cursor-React-Homework4/static/media/5_5.93283ca4.jpg"
+  },
+  {
+    "revision": "b0fb9b992ca1f9b3f2ad36bb260178e8",
+    "url": "/Cursor-React-Homework4/static/media/6_6.b0fb9b99.jpg"
+  },
+  {
+    "revision": "9119e74aaeea07fd71a06d4188695506",
+    "url": "/Cursor-React-Homework4/static/media/7_7.9119e74a.jpg"
+  },
+  {
+    "revision": "34c3b97852a0ecb182c09785b3ee3f65",
+    "url": "/Cursor-React-Homework4/static/media/8_8.34c3b978.jpg"
+  },
+  {
+    "revision": "f99234a58bd6c83df964328ca2df5dec",
+    "url": "/Cursor-React-Homework4/static/media/9_9.f99234a5.jpg"
+  },
+  {
+    "revision": "4907b4d9de8b618d7a3d9d57bf94a13d",
+    "url": "/Cursor-React-Homework4/static/media/abstraktsiia_uzory_detali_130205_1366x768.4907b4d9.jpg"
+  }
+]);
